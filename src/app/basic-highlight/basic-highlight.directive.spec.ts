81ba054264f840277d9f9786f76e789a840f87b5
novita <novitarahmaa11@gmail.com>
2018-11-01 17:40:44 -0700

    first commit

---
+++ src/app/basic-highlight/basic-highlight.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BasicHighlight.directive.spec.tsDirective } from './basic-highlight.directive.spec.ts.directive';
+
+describe('Directive: BasicHighlight.directive.spec.ts', () => {
+  it('should create an instance', () => {
+    const directive = new BasicHighlight.directive.spec.tsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
